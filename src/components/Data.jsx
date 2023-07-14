cc6ae34c7ab39279c7fd3fa11cd27b9c9f4dd921
Meriemchm <chamimeriem2302@gmail.com>
2023-07-14 18:28:29 +0100

    V1-change-contact

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -131,3 +131,28 @@ export const menuItem = [
   },
 
 ];
+
+export const formItem = [
+  {
+    id: 1,
+    name: "name",
+    type: "text",
+    placeholder: "Enter your name",
+    pattern: "[A-Za-z]+",
+    categorie:"input",
+  },
+  {
+    id: 2,
+    name: "email",
+    type: "text",
+    placeholder: "Enter your email",
+    pattern: "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}",
+    categorie:"input",
+  },
+  {
+    id: 3,
+    name: "message",
+    placeholder: "Enter your message",
+    categorie:"textarea",
+  },
+];
