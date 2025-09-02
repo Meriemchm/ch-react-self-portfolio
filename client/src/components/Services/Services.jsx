1b4519520d9781d400ef4fb7e65f55b1bdc85a70
Meriemchm <chamimeriem2302@gmail.com>
2025-09-02 13:33:35 +0100

    create client folder

---
+++ client/src/components/Services/Services.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Service from "./Service";
+import { MdOutlineWorkOutline } from "react-icons/md";
+import { services } from "../../Data/Data";
+import Title from "../Utilities/Title";
+const Services = () => {
+  return (
+    <div name="services" className=" h-full w-full bg-six ">
+      <div className="max-w-screen-lg mx-auto p-4 flex flex-col  w-full h-full text-white">
+        <Title
+          icon={<MdOutlineWorkOutline className="text-white" size={40} />}
+          title="Services"
+          description="There is the services i provide."
+        />
+
+        <div data-aos="fade-up" className="">
+          <Service data={services} Title="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
