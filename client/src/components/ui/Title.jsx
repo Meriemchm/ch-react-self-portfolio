98627fe74e42ab8635783a4d93d2a095689a84cc
Meriemchm <chamimeriem2302@gmail.com>
2026-01-07 15:34:23 +0100

    Refactor layout and typography in Title component for improved readability

--- client/src/components/ui/Title.jsx
+++ client/src/components/ui/Title.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const Title = ({icon,title,description}) => {
   return (
     <div className="mt-8" data-aos="fade-up">
-      <div className="flex gap-3">
-        {/* <div className="bg-primary flex justify-center items-center px-2 rounded-lg ">
-          {icon}
-        </div> */}
-        <p className="md:text-6xl text-3xl text-start text-black font-bold inline Aboreto">{title}</p>
+      <div className="flex flex-col gap-1">
+         {/* <p className="md:text-4xl text-3xl text-start text-black font-bold inline Aboreto">
+          ---
+        </p>  */}
+        <p className="md:text-5xl text-3xl text-start text-black font-bold inline Aboreto">{title}</p>
       </div>{" "}
       <p className="py-6 text-gray-900/60 text-semibold">
         {description}
